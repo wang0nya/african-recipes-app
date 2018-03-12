81ec47398bafdfda9c4e0e8b5d42d83d3c38b685
wang0nya <kwangonya@gmail.com>
2018-03-12 20:33:38 +0300

    remove edit email/pass

--- src/pages/my-profile/my-profile.ts
+++ src/pages/my-profile/my-profile.ts
@@ -50,34 +50,34 @@ export class MyProfilePage {
   updateDOB(birthDate: string): void {
     this.profileProvider.updateDOB(birthDate);
   }
-  updateEmail(): void {
-    let alert: Alert = this.alertCtrl.create({
-      inputs: [{ name: 'newEmail', placeholder: 'Your new email' },
-      { name: 'password', placeholder: 'Your password', type: 'password' }], buttons: [
-        { text: 'Cancel' }, {
-          text: 'Save',
-          handler: data => {
-            this.profileProvider
-            .updateEmail(data.newEmail, data.password)
-            .then(() => { console.log('Email Changed Successfully'); })
-            .catch (error => { console.log('ERROR: ' + error.message); });
-          }
-        }]
-    });
-    alert.present();
-  }
-
-updatePassword(): void {
-  let alert: Alert = this.alertCtrl.create({
-    inputs: [
-      { name: 'newPassword', placeholder: 'New password', type: 'password' }, { name: 'oldPassword', placeholder: 'Old password', type: 'password' }],
-    buttons: [
-      { text: 'Cancel' }, {
-        text: 'Save',
-        handler: data => {
-          this.profileProvider.updatePassword(data.newPassword, data.oldPassword);
-        }
-      }]
-  }); alert.present();
-}
+//   updateEmail(): void {
+//     let alert: Alert = this.alertCtrl.create({
+//       inputs: [{ name: 'newEmail', placeholder: 'Your new email' },
+//       { name: 'password', placeholder: 'Your password', type: 'password' }], buttons: [
+//         { text: 'Cancel' }, {
+//           text: 'Save',
+//           handler: data => {
+//             this.profileProvider
+//             .updateEmail(data.newEmail, data.password)
+//             .then(() => { console.log('Email Changed Successfully'); })
+//             .catch (error => { console.log('ERROR: ' + error.message); });
+//           }
+//         }]
+//     });
+//     alert.present();
+//   }
+//
+// updatePassword(): void {
+//   let alert: Alert = this.alertCtrl.create({
+//     inputs: [
+//       { name: 'newPassword', placeholder: 'New password', type: 'password' }, { name: 'oldPassword', placeholder: 'Old password', type: 'password' }],
+//     buttons: [
+//       { text: 'Cancel' }, {
+//         text: 'Save',
+//         handler: data => {
+//           this.profileProvider.updatePassword(data.newPassword, data.oldPassword);
+//         }
+//       }]
+//   }); alert.present();
+// }
 }
