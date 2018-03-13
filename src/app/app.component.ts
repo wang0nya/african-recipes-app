f6ee8daa3fadbe3f7b09675eae00eba7d154072d
wang0nya <kwangonya@gmail.com>
2018-03-13 13:17:44 +0300

    skip login page if user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,10 +19,19 @@ import { firebaseConfig } from './credentials';
 })
 export class MyApp {
   @ViewChild(Nav) navCtrl: Nav;
-    rootPage:any = LoginPage;
+    rootPage: any;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     firebase.initializeApp(firebaseConfig);
+    const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+      if (!user) {
+        this.rootPage = LoginPage;
+        unsubscribe();
+      } else {
+        this.rootPage = HomePage;
+        unsubscribe();
+      }
+    });
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
