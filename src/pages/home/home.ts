77762098dee7556d4f42834ddbe38ab815dfedcb
wang0nya <kwangonya@gmail.com>
2018-03-13 13:47:51 +0300

    fetch firebase data to card

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,17 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AddRecipePage } from '../add-recipe/add-recipe';
+import { RecipeProvider } from "../../providers/recipe/recipe";
+import { ProfileProvider } from "../../providers/profile/profile";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+public userProfile: any;
+public recipeList: Array<any>;
+  constructor(public navCtrl: NavController, public recipeProvider: RecipeProvider,public profileProvider: ProfileProvider) {
   }
   goToAddRecipe(params){
     if (!params) params = {};
     this.navCtrl.push(AddRecipePage);
+  }
+  ionViewDidLoad() {
+    this.profileProvider.getUserProfile().on("value", userProfileSnapshot => {
+      this.userProfile = userProfileSnapshot.val();
+    });
+    this.recipeProvider.getRecipeList().on("value", recipeListSnapshot => {
+      this.recipeList = []; recipeListSnapshot.forEach(snap => {
+        this.recipeList.push({
+          id: snap.key,
+          name: snap.val().name, ingredients: snap.val().ingredients, community: snap.val().community,
+          servings: snap.val().servings, method: snap.val().method
+        });
+        return false;
+      });
+    });
   }
 }
