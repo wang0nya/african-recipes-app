67bcde6caf050958cf1df7f59d984fe518680518
wang0nya <kwangonya@gmail.com>
2018-04-03 19:41:23 +0300

    sum likes

--- src/pages/my-profile/my-profile.ts
+++ src/pages/my-profile/my-profile.ts
@@ -9,6 +9,7 @@ import { LoginPage } from '../login/login';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 import firebase from 'firebase';
 import { Reference} from '@firebase/database-types';
+import { RecipeProvider } from "../../providers/recipe/recipe";
 
 @Component({
   selector: 'page-my-profile',
@@ -23,24 +24,48 @@ export class MyProfilePage {
   public userProfile: any; public birthDate: string;
   public status: string;
   public followed_count: number;
+  public currentUserMe: any = {};
+  public recipeList: Array<any>;
+  public likes_sum: number;
   constructor(
     public navCtrl: NavController,
     public alertCtrl: AlertController, public authProvider: AuthProvider, public profileProvider: ProfileProvider
-  ,  private camera: Camera) {
+  ,  private camera: Camera,public recipeProvider: RecipeProvider) {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
     this.storageRef = firebase.storage().ref();
     this.imageRef = this.storageRef.child(`${user.uid}/pROFILEpic.jpg`);
     this.userProfile = firebase.database().ref(`/userProfile/${user.uid}/profile`);
     this.userProfileRef = firebase.database().ref(`/userProfile/${user.uid}/profile`);
-  }
-});
+      }
+    });
    }
   ionViewDidLoad() {
     this.profileProvider.getUserProfile().on("value", userProfileSnapshot => {
       this.userProfile = userProfileSnapshot.val(); this.birthDate = userProfileSnapshot.val().birthDate;
       this.status = userProfileSnapshot.val().status;
       this.followed_count = userProfileSnapshot.val().followed_count;
+    });
+    this.profileProvider.getMyUserProfile().on("value", userSnapshot => {
+      this.currentUserMe = userSnapshot.val();
+      this.currentUserMe.id = userSnapshot.key;
+      this.recipeProvider.recipeListRef
+      .once("value", recipeListSnapshot => {
+        this.recipeList = [];
+        this.likes_sum = 0;
+        recipeListSnapshot.forEach(snap => {
+          this.recipeProvider.recipeListRef
+          if (snap.val().chef.uid==this.currentUserMe.id){
+            console.log('jackpot!', snap.val().chef.uid);
+            this.likes_sum += snap.val().likes_count;
+            console.log("total likes", this.likes_sum);
+          }
+          else {
+            console.log("not yours");
+          }
+        return false;
+        });
+      });
     });
   }
   logOut(): void {
